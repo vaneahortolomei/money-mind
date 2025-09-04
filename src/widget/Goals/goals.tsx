4ce8f90dd16234c2f3d4fc96bc7f7e97608e7f98
Vanea <hortolomei1989@gmail.com>
2025-09-04 15:17:09 +0300

    :zap: Finished with dashboard elements

--- src/widget/Goals/goals.tsx
+++ src/widget/Goals/goals.tsx
@@ -1,5 +1,13 @@
+import { Progress, Title, Stack, Text, Flex } from '@mantine/core';
+
 export const Goals = () => {
     return (
-        <div>Goals</div>
+        <Stack>
+            <Title>Goals</Title>
+            <Flex direction={"column"} gap={10}>
+                <Text>Save 1000$ by December</Text>
+                <Progress size="sm" value={33}/>
+            </Flex>
+        </Stack>
     )
 }
--- src/widget/SpendingPie/spending-pie.tsx
+++ src/widget/SpendingPie/spending-pie.tsx
@@ -1,3 +1,4 @@
+import {Title, Stack, List, Box, Text, Flex} from '@mantine/core';
 import {PieChart} from '@mantine/charts';
 
 
@@ -38,6 +39,21 @@ const chartData: ChartDataValues[] = [
 
 export const SpendingPie = () => {
     return (
-        <PieChart key={'1'} data={chartData} withTooltip tooltipDataSource="segment" mx="auto"/>
+        <Stack>
+            <Title>Budgets</Title>
+            <PieChart key={'1'} data={chartData} withTooltip tooltipDataSource="segment" mx="auto"/>
+            <List>
+                {chartData.map((item) => (
+                    <List.Item key={item.name} icon={
+                        <Box bg={item.color} w={20} h={20} style={{borderRadius: '50%'}}/>
+                    }>
+                        <Flex justify={'space-between'}>
+                            <Text>{item.name}</Text>
+                            <Text>{item.value}%</Text>
+                        </Flex>
+                    </List.Item>
+                ))}
+            </List>
+        </Stack>
     )
 }
